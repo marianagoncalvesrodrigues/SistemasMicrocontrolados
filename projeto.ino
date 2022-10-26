4980c37559f560ed35a61c3e3c2902224207fa33
Mariana Gonçalves Rodrigues <mary.judo@gmail.com>
2022-10-26 10:58:18 -0300

    Create projeto.ino

---
+++ projeto.ino
@@ -0,0 +1,52 @@
+#include <Servo.h> 
+#include <SoftwareSerial.h> //biblioteca bluetooth
+
+SoftwareSerial SerialBT(10, 11); // RX, TX do Arduino
+
+Servo meuservo;
+int ang = 0;
+int sensorAgua = 8;
+int botao = 7;
+
+void setup() {
+  SerialBT.begin(9600);
+  
+  meuservo.attach(9);  // Pino usado para conectar o servo
+  pinMode(sensorAgua, INPUT);
+  pinMode(botao, INPUT_PULLUP);
+}
+
+void estendeVaral(){
+  for (ang = 0; ang <= 360; ang += 1) { // Vai de 0º até 360º
+    meuservo.write(ang);              // Comando que envia o sinal para o servo rotacionar
+    delay(15);                       // Espera 15ms 
+  }
+  int msgEstendeVaral = 1;
+  SerialBT.write(msgEstendeVaral);
+}
+
+void recolheVaral(){
+  for (ang = 360; ang >= 0; ang -= 3) { // Vai de 360º até 0º (+ rápido)
+    meuservo.write(ang);              // Comando que envia o sinal para o servo rotacionar
+    delay(15);                       // Espera 15ms 
+  }
+  int msgEstendeVaral = 0;
+  SerialBT.write(msgEstendeVaral);  
+}
+
+void loop() {
+  if( digitalRead(sensorAgua) == LOW) {
+    recolheVaral();
+  }
+  //if(digitalRead(botao) == LOW){
+   // estendeVaral(); 
+  //}
+  if(SerialBT.available()){
+    int data = SerialBT.read();
+    if(data == 1){
+      estenderVaral();
+    }else if(data == 0){
+      recolherVaral();
+    }
+  }
+}
