1e3349763fe7596228e67aa1216bd52f46990437
Mariana Gonçalves Rodrigues <mary.judo@gmail.com>
2022-11-16 10:56:31 -0300

    Update projeto.ino

--- projeto.ino
+++ projeto.ino
@@ -6,42 +6,50 @@ SoftwareSerial SerialBT(10, 11); // RX, TX do Arduino
 Servo meuservo;
 int ang = 0;
 int sensorAgua = 8;
-int botao = 7;
 int msgEstendeVaral = 0;
-
+int motor1Pin1 = 5; 
+int motor1Pin2 = 6;
+int agua = A0;
 
 void setup() {
   SerialBT.begin(9600);
+  Serial.begin(9600);
   meuservo.attach(9);  // Pino usado para conectar o servo
-  pinMode(sensorAgua, INPUT);
-  pinMode(botao, INPUT_PULLUP);
+  pinMode(agua, INPUT);
+  pinMode(motor1Pin1, OUTPUT);
+  pinMode(motor1Pin2, OUTPUT);
 }
 
 void estendeVaral(){
-  for (ang = 0; ang <= 360; ang += 1) { // Vai de 0º até 360º
-    meuservo.write(ang);              // Comando que envia o sinal para o servo rotacionar
-    delay(15);                       // Espera 15ms 
-  }
+  analogWrite(motor1Pin1, 120);//frente
+  digitalWrite(motor1Pin2, LOW);
+  delay(2000);
+  digitalWrite(motor1Pin1, LOW);
   msgEstendeVaral = 1;
   SerialBT.write(msgEstendeVaral);
 }
 
 void recolheVaral(){
-  for (ang = 360; ang >= 0; ang -= 3) { // Vai de 360º até 0º (+ rápido)
-    meuservo.write(ang);                // Comando que envia o sinal para o servo rotacionar
-    delay(15);                          // Espera 15ms 
-  }
+  analogWrite(motor1Pin2, 120);//marcha ré
+  digitalWrite(motor1Pin1, LOW);
+  delay(2000);
+  digitalWrite(motor1Pin2, LOW);
   msgEstendeVaral = 0;
   SerialBT.write(msgEstendeVaral);  
 }
 
 void loop() {
-  if( digitalRead(sensorAgua) == LOW) {
+  int aguinha = analogRead(agua);
+  Serial.println(aguinha);
+  if( analogRead(agua) < 512 && msgEstendeVaral) {
     recolheVaral();
-  }
-  //if(digitalRead(botao) == LOW){
-   // estendeVaral(); 
-  //}
+    if(SerialBT.available()){
+      int dat = SerialBT.read();
+      if(dat == 1){
+        estendeVaral();
+      }
+    }
+  } 
   if(SerialBT.available()){
     int data = SerialBT.read();
     if(data == 1){
